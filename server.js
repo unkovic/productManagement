af1a7590342791efa038542b4785bb130ee4042f
Unkovic Nemanja <unkovic.nemanja@yahoo.com>
2015-11-14 02:44:33 +0100

    Change in server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var express = require("express"),
     bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
-    hostname = process.env.HOSTNAME || 'localhost',
-    port = parseInt(process.env.PORT, 10) || 4567,
+    port = process.env.PORT || 4567,
     publicDir = process.argv[2] || __dirname + '/public';
 
 app.get("/", function (req, res) {
@@ -23,4 +22,4 @@ app.use(errorHandler({
 }));
 
 console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
-app.listen(port, hostname);
+app.listen(port);
